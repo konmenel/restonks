2333f2e0a3a22915c2c145ee3b772da57c78910d
Constantinos Menelaou <konmenel@gmail.com>
2025-04-14 02:28:19 +0100

    First gui comming

--- restonks/gui.py
+++ restonks/gui.py
@@ -1,8 +1,36 @@
 #!/bin/env python3
+import os
+import sys
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile
+
+class Restonks:
+    def __init__(self):
+        # Load the .ui file
+        base, _ = os.path.split(__file__)
+        ui_file_name = os.path.join(base, "../ui/main.ui")
+        ui_file = QFile(ui_file_name)
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        # Connect button callbacks
+        self.window.addButton.clicked.connect(self.handle_add_position)
+
+    # Define callbacks
+    def handle_add_position(self):
+        print("Add Position button clicked!")
 
 
 def main() -> int:
-    return 0
+    app = QApplication(sys.argv)
+
+    restonks = Restonks()
+    restonks.window.show()
+
+    return app.exec()
 
 
 if __name__ == "__main__":
