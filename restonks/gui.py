4cc49f79d032b5951ba9da58b6f693f85256602c
Constantinos Menelaou <konmenel@gmail.com>
2025-04-20 18:47:10 +0100

    Add calba

--- restonks/gui.py
+++ restonks/gui.py
@@ -2,8 +2,13 @@
 import os
 import sys
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QTableWidgetItem
 from PySide6.QtCore import QFile
+
+try:
+    from . import lib
+except ImportError:  # TODO: Remove this when packaging
+    import lib
 
 # TODO: Add callbacks
 # TODO: Add button to accept the API keys
@@ -11,6 +16,7 @@ from PySide6.QtCore import QFile
 # TODO: Save last configuration of weights for next session
 # TODO: Create second tab/dock for results
 # TODO: Pull list of securities on startup.
+
 
 class Restonks:
     def __init__(self):
@@ -24,15 +30,40 @@ class Restonks:
         ui_file.close()
 
         # Connect button callbacks
-        self.window.addButton.clicked.connect(self.handle_add_position)
+        self.window.refreshButton.clicked.connect(self.handle_refresh)
 
     # Define callbacks
-    def handle_add_position(self):
-        print("Add Position button clicked!")
+    def handle_refresh(self):
+        positions = lib.get_all_positions()
+        print(positions)
+        portfolio_table = self.window.portfolioTable
+        portfolio_table.setRowCount(len(positions))
+
+        for i, (ticker, position) in enumerate(positions.items()):
+            portfolio_table.setItem(i, 0, QTableWidgetItem(ticker))
+            portfolio_table.setItem(
+                i, 1, QTableWidgetItem(f"{position['market_price']:.2f}")
+            )
+            portfolio_table.setItem(i, 2, QTableWidgetItem(f"{position['shares']:d}"))
+            portfolio_table.setItem(
+                i, 3, QTableWidgetItem(f"{position['market_value']:.2f}")
+            )
+            portfolio_table.setItem(i, 4, QTableWidgetItem(f"{position['weight']:.2%}"))
+            portfolio_table.setItem(
+                i, 5, QTableWidgetItem(f"{position['target_weight']:.2%}")
+            )
+        # self.window.portfolioTable.show()
 
 
 def main() -> int:
     app = QApplication(sys.argv)
+
+    # TODO: needs to be independent of current weights and api key file
+    lib.config.initialise(
+        api_key_file="tradernet.ini",
+        weights_file="weights.toml",
+        investment_amount=1000,
+    )
 
     restonks = Restonks()
     restonks.window.show()
